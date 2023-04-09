eb3f8e29e840389c63b2c4e2447f7af2f2b0defe
Folexy13 <folajimiopeyemisax13@gmail.com>
2023-04-09 14:33:26 +0100

    cleaning up code ...

--- src/knexFile.js
+++ src/knexFile.js
@@ -1,13 +1,14 @@
+import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from "../config";
 const { Knex } = require("knex");
 
 const knexConfig = {
   client: "mysql2",
   connection: {
-    host: "db4free.net",
-    user: "folayemi",
-    password: "Oluwabunmi13#",
-    database: "folatestdb",
-    port: 3306,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME,
+    port: DB_PORT,
   },
 };
 
